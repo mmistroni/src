c2230d4d8e60b038a04b43e5d718ff6296e6063a
mmistroni <mmistroni@gmail.com>
2024-07-18 05:55:23 +0100

    added timeout

--- greetings/coroutines_select_test.go
+++ greetings/coroutines_select_test.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 // TestHelloName calls greetings.Hello with a name, checking
@@ -19,6 +20,25 @@ func TestRunningSelect(t *testing.T) {
 			fmt.Println("Received:" + msg)
 		case msg := <- channel2:
 			fmt.Println("Received:" + msg)
+
+	}
+
+}
+
+func TestRunningSelectWithTimeout(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go ping1(channel1)
+	go ping2(channel2)
+
+	select {
+		case msg := <- channel1:
+			fmt.Println("Received:" + msg)
+		case msg := <- channel2:
+			fmt.Println("Received:" + msg)
+		case <- time.After(500 * time.Millisecond):
+			fmt.Println("No message received after 500ms")
 
 	}
 
